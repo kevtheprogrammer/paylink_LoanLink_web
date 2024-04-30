e4735ce618741a0e94734b5ea4603d4b793796e6
Francischiputa02 <francisc@paylinkzm.com>
2024-04-30 10:10:00 +0200

    collected

--- core/views.py
+++ core/views.py
@@ -3,7 +3,7 @@ from account.models import *
 from django.http import HttpResponse
 
 def ClientListView(request):
-    clients = User.objects.all()
+    clients = ClientProfile.objects.all()
     print(clients)
     return render(request, 'core/clients.html', {'clients': clients})
 
